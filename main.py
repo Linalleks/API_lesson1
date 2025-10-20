d6e62f399ad7095c7020eabeb1c154dd54143411
Ангелина Петрова <linalleks@gmail.com>
2025-10-20 19:52:00 +0700

    Remove extra feature & code

--- main.py
+++ main.py
@@ -2,68 +2,22 @@ import requests
 
 
 def main():
-    locations_params = (
-        (
-            'san francisco',
-            'San francisco (Сан Франциско)',
-            {
-                'u': '',
-                'M': '',
-                'n': '',
-                'q': '',
-                'T': '',
-                'lang': 'en'
-            }
-        ),
-        (
-            'london',
-            'London (Лондон)',
-            {
-                'm': '',
-                'M': '',
-                'n': '',
-                'q': '',
-                'T': '',
-                'lang': 'en'
-            },
-        ),
-        (
-            'svo',
-            'Аэропорт Шереметьево (Sheremetyevo Airport, код ICAO: SVO)',
-            {
-                'm': '',
-                'M': '',
-                'n': '',
-                'q': '',
-                'T': '',
-                'lang': 'ru'
-            },
-        ),
-        (
-            'Череповец',
-            'Череповец (Cherepovets)',
-            {
-                'm': '',
-                'M': '',
-                'n': '',
-                'q': '',
-                'T': '',
-                'lang': 'ru'
-            },
-        )
-    )
+    locations = ('london', 'svo', 'Череповец')
+    display_params = {
+        'm': '',
+        'M': '',
+        'n': '',
+        'q': '',
+        'T': '',
+        'lang': 'ru'
+    }
 
-    print('Номер : Локация офиса')
-
-    for location in locations_params:
-        print(f'    {locations_params.index(location)} : {location[1]}')
-
-    id_location = int(input('\nУкажите номер локации вашего офиса: '))
     url_template = 'https://wttr.in/{}'
-    url = url_template.format(locations_params[id_location][0])
-    response = requests.get(url, params=locations_params[id_location][2])
-    response.raise_for_status()
-    print(response.text)
+    for location in locations:
+        url = url_template.format(location)
+        response = requests.get(url, params=display_params)
+        response.raise_for_status()
+        print(response.text)
 
 
 if __name__ == '__main__':
